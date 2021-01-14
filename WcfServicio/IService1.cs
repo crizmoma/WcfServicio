64e1486c651a80a8fbc737a24d401bc1a7e31b55
crizmoma <criztalmontelongo1998@gmail.com>
2021-01-13 20:33:31 -0600

    Agregar archivos de proyecto.

---
+++ WcfServicio/IService1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfServicio
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IService1" en el código y en el archivo de configuración a la vez.
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+        Peliculas CrearPeliculas();
+
+        [OperationContract]
+        Alumnos CrearAlumnos();
+
+        [OperationContract]
+        Alumnos CrearLibros();
+
+        [OperationContract]
+        double ConvertirDolares(double pesos);
+        [OperationContract]
+        string GetData(int value);
+
+        [OperationContract]
+        Peliculas GetDatosPeliculas(Peliculas pelicula);
+        [OperationContract]
+        Alumnos GetDatosAlumnos(Alumnos alumno);
+        [OperationContract]
+        Libros GetDatosLibros(Libros libro);
+        [OperationContract]
+        CompositeType GetDataUsingDataContract(CompositeType composite);
+
+        // TODO: agregue aquí sus operaciones de servicio
+    }
+
+
+    // Utilice un contrato de datos, como se ilustra en el ejemplo siguiente, para agregar tipos compuestos a las operaciones de servicio.
+    [DataContract]
+    public class Peliculas
+    {
+        string nombre;
+        string director;
+        float duracion;
+        int clasificacion;
+
+        [DataMember]
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+        [DataMember]
+        public string Director
+        {
+            get { return director; }
+            set { director = value; }
+        }
+        [DataMember]
+        public float Duración
+        {
+            get { return duracion; }
+            set { duracion = value; }
+        }
+        [DataMember]
+        public int Clasificación
+        {
+            get { return clasificacion; }
+            set { clasificacion = value; }
+        }
+    }
+    [DataContract]
+    public class Alumnos
+    {
+        string nombre;
+        string carrera;
+        float nocontrol;
+        int promedio;
+
+        [DataMember]
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+        [DataMember]
+        public string Carrera
+        {
+            get { return carrera; }
+            set { carrera = value; }
+        }
+        [DataMember]
+        public float Nocontrol
+        {
+            get { return nocontrol; }
+            set { nocontrol = value; }
+        }
+        [DataMember]
+        public int Promedio
+        {
+            get { return promedio; }
+            set { promedio = value; }
+        }
+    }
+    public class Libros
+    {
+        string nombre;
+        string autor;
+        float id;
+
+        [DataMember]
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+        [DataMember]
+        public string Autor
+        {
+            get { return autor; }
+            set { autor = value; }
+        }
+        [DataMember]
+        public float Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+    }
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+}
---
+++ WcfServicio/Service1.svc.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfServicio
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Service1" en el código, en svc y en el archivo de configuración.
+    // NOTE: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione Service1.svc o Service1.svc.cs en el Explorador de soluciones e inicie la depuración.
+    public class Service1 : IService1
+    {
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+        public double ConvertirDolares(double pesos)
+        {
+
+            return pesos * 20.52;
+        }
+        public Peliculas GetDatosPeliculas(Peliculas pelicula)
+        {
+            return pelicula;
+        }
+        public Peliculas CrearPeliculas()
+        {
+            Peliculas pelicula = new Peliculas
+            {
+                Nombre = "Norbit",
+                Clasificación = 7,
+                Director = "Alejandro",
+                Duración = 2
+            };
+            return pelicula;
+        }
+        public Alumnos CrearAlumnos()
+        {
+            Alumnos alumno = new Alumnos
+            {
+                Nombre = "Cristal Montelongo",
+                Carrera = "informática",
+                Nocontrol = 17030096,
+                Promedio = 89
+            };
+            return alumno;
+        }
+        public Libros CrearLibros()
+        {
+            Libros libro = new Libros
+            {
+                Nombre = "Los sueos",
+                Autor = "wiliam",
+                Id = 123
+            };
+            return libro;
+        }
+        public Alumnos GetDatosAlumnos(Alumnos alumno)
+        {
+            return alumno;
+        }
+        public Libros  GetDatosLibros(Libros libro)
+        {
+            return libro;
+        }
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+
+        Alumnos IService1.CrearLibros()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
